b2128212af6bf5a30f9e67d2066fba898b9635c0
caio pereira <caiomp87@gmail.com>
2022-09-25 20:53:26 -0300

    create controllers

---
+++ controllers/crypto.go
@@ -0,0 +1,72 @@
+package controllers
+
+import (
+	"errors"
+	"net/http"
+	"strconv"
+
+	datastore "github.com/caiomp87/crypto-votes-datastore/postgres"
+	"github.com/caiomp87/crypto-votes-entities/models"
+	"github.com/gin-gonic/gin"
+)
+
+func ListCryptos(ctx *gin.Context) {
+	cryptos, err := datastore.CryptoDatastore.FindAll()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, cryptos)
+}
+
+func GetCrypto(ctx *gin.Context) {
+	id := ctx.Param("id")
+	if id == "" {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": errors.New("id field is required"),
+		})
+		return
+	}
+
+	convertedID, err := strconv.Atoi(id)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	crypto, err := datastore.CryptoDatastore.FindByID(convertedID)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, crypto)
+}
+
+func CreateCrypto(ctx *gin.Context) {
+	var crypto *models.Crypto
+	if err := ctx.BindJSON(&crypto); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	if err := datastore.CryptoDatastore.Create(crypto); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, gin.H{
+		"message": "crypto created successfully",
+	})
+}
