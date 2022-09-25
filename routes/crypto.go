4267bd29e18b7fba7b76ec7dc35768216deba4f3
caio pereira <caiomp87@gmail.com>
2022-09-25 20:53:06 -0300

    create function to add crypto routes in gin engine

---
+++ routes/crypto.go
@@ -0,0 +1,18 @@
+package routes
+
+import (
+	"github.com/caiomp87/crypto-votes-api/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func AddCryptoRoutes(r *gin.Engine) {
+	v1 := r.Group("/api/v1")
+	{
+		crypto := v1.Group("/crypto")
+		{
+			crypto.GET("/", controllers.ListCryptos)
+			crypto.GET("/:id", controllers.GetCrypto)
+			crypto.POST("/", controllers.CreateCrypto)
+		}
+	}
+}
